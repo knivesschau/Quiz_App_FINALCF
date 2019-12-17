b0cfa19aa2086bb6a29183e417a1158e56ef4d6c
Catrina Fuentes <cat.fuentes93@gmail.com>
2019-12-16 23:43:17 -0800

    basic functionality works

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const store = {
+const STORE = {
   questions: [
     {
       question: "What is the name of Link's fairy in Ocarina of Time?",
@@ -10,7 +10,7 @@ const store = {
         'Tatl',
         'Saria'
       ],
-      correctAnswer: 'Navi'
+      correctAnswer: "Navi"
     },
     {
       question: "Which of the following is NOT a usable item in A Link to the Past?",
@@ -93,7 +93,7 @@ const store = {
       correctAnswer: 'Shadow Temple'
     },
     {
-      question: "In A Link to the Past, what animal does Link turn into when he first enters the Dark World?"
+      question: "In A Link to the Past, what animal does Link turn into when he first enters the Dark World?",
       answers: [
         'A white dove',
         'A pink rabbit',
@@ -106,20 +106,24 @@ const store = {
   questionNumber: 0,
   currentScore: 0,
   quizStart: false
-}
+};
 
-//this function increments the question number. 
+//this function increments the question number as the user goes through each question.
 function questionTracker() {
-  questionNumber++;
+  console.log('questionTracker ran');
+  STORE.questionNumber++;
 }
 
 //this function increments the user score if they answer correctly.
 function scoreTracker() {
-  currentScore++;
+  console.log('scoreTracker ran');
+  STORE.currentScore++;
 }
 
 //this function holds the HTML for the quiz's landing page. 
 function homePage() {
+  console.log('homePage ran');
+  
   const homePageHTML = 
   `<header role="banner">
   <h1 class="title-homepage"> The Legend of Zelda Trivia Quiz </h1>
@@ -139,48 +143,141 @@ function homePage() {
 
 //this function holds the HTML for the quiz's final results page. 
 function resultPage(){
+  console.log('resultPage ran');
+  
   const resultPageHTML = 
   `<header role="banner">
   <h1 class="title-results">Final Results</h1>
   <h2 class="final-score">Your final score was ${STORE.currentScore} out of 10!</h2>
   </header>
-  <main id="results-page">
+  <main id="results-page">`
+  
+  const restartButton = 
+  `<fieldset id="restart-button">
   <form id="end-page">
-  <fieldset id="restart-button">
   <input type="button" id="restart-quiz" aria-label="Restart Quiz Button" value="Click to Try Again!"></input>
-  `
+  </form>
+  </fieldset>
+  </main>`
 
-  if (STORE.currentScore <= 5) {
-    $('body').html(resultPageHTMl).text(`<p id="above-5">Nice job! You're a <i>Zelda</i> whiz!</p>`);
+  if (STORE.currentScore >= 5) {
+    $('body').html(resultPageHTML + `<p class="above-5">Nice job! You're a Zelda whiz!</p>` + restartButton);
   }
-  else if (STORE.currentScore > 5) {
-    $('body').html(resultsPageHTML).text(`<p id="below-5">It looks like you need to brush up on your <i>Zelda</i> knowledge!`);
+  else if (STORE.currentScore < 5) {
+    $('body').html(resultPageHTML + `<p class="below-5">It looks like you need to brush up on your Zelda knowledge!</p>` + restartButton);
   }
 }
 
 //this function renders the HTML for all questions/answer choices, as well as the user's score/question progress.
 function questionGenerator() {
+  console.log('questionGenerator ran')
 
+  const questionText = 
+  `<main class = "main-quiz">
+  <fieldset id="quiz-questions-answers">
+  <form id="quiz-form">
+  <legend id="question-text">${STORE.questions[STORE.questionNumber].question}</legend>`
+
+  const answerText = STORE.questions[STORE.questionNumber].answers.map(answer => {
+    return `<input type="radio" name="answer-selections" aria-label="Select Answer" value="${answer}">${answer}</input>`
+  }); 
+
+  const quizButtons = 
+  `<input type="button" id="next-question" aria-label="Next Question Button" value="Next Question"></input>
+  <input type="button" id="submit-button" aria-label="Submit Answer Button" value="Submit Answer"></input>
+  <input type="button" id="show-results" aria-label="Show Results Button" value="Show Me My Results!"></input>`
+
+  const infoTrackers = 
+  `
+  <p id="question-tracker">Question ${STORE.questionNumber + 1} out of 10</p>
+  <p id="score-tracker">Your current score is: ${STORE.currentScore} out of 10</p>
+  </form>
+  </fieldset>
+  </main>`
+
+$('body').html(questionText + answerText.join('') + quizButtons + infoTrackers)
+$("#next-question").hide(); 
+$("#show-results").hide();
+}
+
+//this function toggles the buttons on every question page depending on user interaction.
+function buttonSwitch() {
+  $("#no-answer").remove();
+  $("#submit-button").hide();
+  
+  if (STORE.questionNumber < 10) {
+      $("#next-question").show();
+    }
+  else if (STORE.questionNumber === 10) {
+    $("#show-results").show();
+  }
 }
 
 //this function checks the user's answers against the STORE with textual feedback if they are right/wrong. if they don't answer, they will be prevented from progressing.
 function answerChecker() {
-
-}
+  console.log('answerChecker ran')
+  $("#no-answer").remove();
+  const answerChoice = $("input[name='answer-selections']:checked").val();
+  if (answerChoice === undefined) {
+    $("#next-question").before(`<p id="unanswered">Please select an answer.</p>`);
+  }
+  else if (answerChoice === STORE.questions[STORE.questionNumber].correctAnswer) {
+    questionTracker();
+    scoreTracker(); 
+    buttonSwitch();
+    $("#next-question").before(`<p id="correct"> That's right! Your current score is ${STORE.currentScore} out of 10.</p>`)
+  }
+  else if (answerChoice !== STORE.questions[STORE.questionNumber].correctAnswer) {
+    questionTracker(); 
+    buttonSwitch(); 
+    $("#next-question").before(`<p id="incorrect"> Not quite! The correct answer is ${STORE.questions[STORE.questionNumber - 1].correctAnswer}. 
+    Your current score is ${STORE.currentScore} out of 10.</p>`)
+  }
+};
 
 //this function implements button functionality to load immediately into the DOM to work in tandem with the quizRender() function. 
 function buttonInitialize() {
-  $(document).on("click", "#start-quiz", function(event) {
-    STORE.quizStart === true; 
+console.log('buttonInitialize ran');
+$(document).on("click", "#start-quiz", function(event) {
+    STORE.quizStart = true; 
     quizRender(); 
   }); 
+
+$(document).on("click","#submit-button", function(event) {
+  answerChecker();
+  });
+
+$(document).on("click","#next-question", function(event) {
+  quizRender();
+  });
+
+$(document).on("click","#show-results", function(event) {
+  quizRender();
+  });
+
+$(document).on("click","#restart-quiz", function(event) {
+  STORE.quizStart = false;
+  STORE.questionNumber = 0;
+  STORE.currentScore = 0;
+  homePage();
+  });
+
+quizRender();
 }
 
 //this function renders the appropriate quiz HTML page based on several checks on the user's progress AND their button interactions. 
 function quizRender() {
+  console.log('quizRender ran');
   if (STORE.quizStart === false) {
-    homePage(); 
+    homePage();
+  }
+  else if (STORE.questionNumber >= 0 && STORE.questionNumber < 10) {
+    questionGenerator();
+  }
+  else if (STORE.questionNumber >= 10) {
+    resultPage(); 
+    scoreTracker();
   }
 }
 
-$(homePage);
+$(buttonInitialize);
