612996da6319987402f3dee8149470c498264840
Catrina Fuentes <cat.fuentes93@gmail.com>
2019-12-16 18:17:08 -0800

    started working on functionality

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const store = {
   questions: [
     {
@@ -52,13 +54,57 @@ const store = {
     },
   ],
   questionNumber: 0,
-  currentScore: 0
-};
+  currentScore: 0,
+  quizStart: false
+}
 
+//this function increments the question number. 
 function questionTracker() {
   questionNumber++;
 }
 
+//this function increments the user score if they answer correctly.
 function scoreTracker() {
   currentScore++;
 }
+
+//this function holds the HTML for the quiz's landing page. 
+function homePage() {
+  const homePageHTML = 
+  `<header role="banner">
+  <h1 class="title-homepage"> The Legend of Zelda Trivia Quiz </h1>
+  <h2 class="secondary-text"> How much do you know about this beloved series? </h2>
+  </header>
+
+  <main id ="home-page">
+  <form id ="start-page">
+  <fieldset id ="starter-button">
+  <input type="button" id="start-quiz" aria-label="Start Quiz Button" value="Let's Find Out!"></input>
+  </fieldset>
+  </form> 
+  </main>`
+
+  $('body').html(homePageHTML);
+}
+
+//this function holds the HTML for the quiz's landing page. 
+function resultPage(){
+  
+}
+
+//this function implements button functionality to load immediately into the DOM to work in tandem with quizRender(). 
+function buttonInitialize() {
+  $(document).on("click", "#start-quiz", function(event) {
+    STORE.quizStart === true; 
+    quizRender(); 
+  }); 
+}
+
+//this function renders the appropriate quiz HTML page based on several checks on the user's progress AND their button interactions. 
+function quizRender() {
+  if (STORE.quizStart === false) {
+    homePage(); 
+  }
+}
+
+$(homePage);
